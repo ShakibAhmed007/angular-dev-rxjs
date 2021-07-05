3c7d6ee801a4914199faee078b4c831b47cf57f5
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-07-05 13:07:33 +0600

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { HelloComponent } from './hello.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material/tabs';
 import { PromiseExampleComponent } from './promise-example/promise-example.component';
+import { ObservableExampleComponent } from './observable-example/observable-example.component';
 
 @NgModule({
   imports: [BrowserModule, BrowserAnimationsModule, FormsModule, MatTabsModule],
-  declarations: [AppComponent, HelloComponent, PromiseExampleComponent],
+  declarations: [AppComponent, HelloComponent, PromiseExampleComponent, ObservableExampleComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
