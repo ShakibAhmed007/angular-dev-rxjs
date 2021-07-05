f78bda59ac095943b9fe81689c4921753c49c9ac
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-07-05 13:15:01 +0600

    1 file modified

--- src/app/observable-example/observable-example.component.ts
+++ src/app/observable-example/observable-example.component.ts
@@ -1,4 +1,6 @@
+
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-observable-example',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./observable-example.component.css']
 })
 export class ObservableExampleComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    const observable = new Observable(subscriber => {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+      subscriber.next(4);
 
+      setTimeout(() => {
+        subscriber.next(5);
+      }, 1000);
+    });
+
+    observable.subscribe({
+      next(x) {
+        console.log(x);
+      },
+      error(x) {
+        console.log(x);
+      },
+      complete() {
+        console.log('Done');
+      }
+    });
+  }
 }
