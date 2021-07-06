b7e2aff3e5682218ec08d4064e22a2f4613d0b11
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-07-06 15:37:22 +0600

    1 file modified

--- src/app/observable-example/observable-example.component.ts
+++ src/app/observable-example/observable-example.component.ts
@@ -1,35 +1,33 @@
-import { error } from '@angular/compiler/src/util';
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-observable-example',
   templateUrl: './observable-example.component.html',
   styleUrls: ['./observable-example.component.css']
 })
-export class ObservableExampleComponent implements OnInit {
-  obs = new Observable(subscriber => {
-    try {
-      subscriber.next(1);
-      subscriber.next(2);
-      subscriber.next(3);
-      subscriber.next(4);
-      setTimeout(() => {
-        subscriber.next(5); // this will not execute
-      }, 1000);
-      subscriber.complete();
-    } catch (err) {
-      subscriber.next('error occured ');
-    }
-  });
+export class ObservableExampleComponent implements OnInit, OnDestroy {
+  private obsSubscription: Subscription;
 
   constructor() {}
 
   ngOnInit() {}
 
   basicObservable() {
+    const obs = new Observable(subscriber => {
+      let count = 0;
+      try {
+        setInterval(() => {
+          subscriber.next(count);
+          count++;
+        }, 1000);
+      } catch (err) {
+        subscriber.next('error occured ');
+      }
+    });
+
     console.log('just before subscribe');
-    const subscription = this.obs.subscribe({
+    this.obsSubscription = obs.subscribe({
       next(x) {
         console.log(x);
       },
@@ -41,8 +39,10 @@ export class ObservableExampleComponent implements OnInit {
       }
     });
     console.log('just after subscribe');
-    
+  }
+
+  ngOnDestroy() {
     // Unsubscribe an observable, ngOnDestroy
-    // subscription.unsubscribe();
+    this.obsSubscription.unsubscribe();
   }
 }
