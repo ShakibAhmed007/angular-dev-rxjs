3b55f8755ced2e75ecf81426985aa1f54b8d627f
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-07-08 12:42:03 +0600

    1 file modified

--- src/app/operator-example/operator-example.component.ts
+++ src/app/operator-example/operator-example.component.ts
@@ -39,14 +39,16 @@ export class OperatorExampleComponent implements OnInit {
     const a = [1, 3, 5, 7];
     const b = [2, 4, 6, 8];
     const ab = combineLatest([a, b]).subscribe(res => {
-      console.log(res);
+      // console.log(res);
     });
 
     // using timer
-    const timer1 = timer(0, 1000);
-    const timer2 = timer(500, 100);
-    const timerObs = combineLatest([timer1, timer2]).subscribe(res => {
-      console.log(res);
-    });
+    const timer1 = timer(0, 5000);
+    const timer2 = timer(500, 10000);
+    const timerObs = combineLatest([timer1, timer2])
+      .pipe(map((t1, t2) => 'timer 1: ' + t1 + ' , timer 2 : ' + t2))
+      .subscribe(res => {
+        console.log(res);
+      });
   }
 }
