2cd014e9ef41879d276442eebd793943efe0c6d3
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-07-08 12:26:56 +0600

    2 files modified

--- src/app/operator-example/operator-example.component.ts
+++ src/app/operator-example/operator-example.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Subscription } from 'rxjs';
 import { interval } from 'rxjs';
+import { combineLatest } from 'rxjs';
+import { timer } from 'rxjs';
 import { map, take } from 'rxjs/operators';
 
 @Component({
@@ -33,7 +35,18 @@ export class OperatorExampleComponent implements OnInit {
     this.obsSubscription.unsubscribe();
   }
 
-  combineLatestExample(){
-    
+  combineLatestExample() {
+    const a = [1, 3, 5, 7];
+    const b = [2, 4, 6, 8];
+    const ab = combineLatest([a, b]).subscribe(res => {
+      console.log(res);
+    });
+
+    // using timer
+    const timer1 = timer(0, 1000);
+    const timer2 = timer(500, 100);
+    const timerObs = combineLatest([timer1, timer2]).subscribe(res => {
+      console.log(res);
+    });
   }
 }
